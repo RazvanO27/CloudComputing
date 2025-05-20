c9f90a7f1c140deb366b435b6d36b0159778b643
RazvanO27 <you@example.com>
2025-05-20 22:34:36 +0300

    message

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,29 @@
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <header style={{ 
+        backgroundColor: "#f0f0f0", 
+        padding: "20px", 
+        textAlign: "center", 
+        borderBottom: "2px solid #0070f3" 
+      }}>
+        <h1 style={{ 
+          margin: 0, 
+          color: "#0070f3", 
+          fontSize: "2rem" 
+        }}>
+          Gym Notes
+        </h1>
+      </header>
+
+      <main style={{ padding: "20px" }}>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
+
+export default MyApp;
+
